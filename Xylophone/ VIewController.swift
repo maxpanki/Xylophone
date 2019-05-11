2d54fe193398889dd0fdb5818d6a2344cc4ceaff
maxpanki <maxrepan@gmail.com>
2019-05-12 00:21:32 +0200

    Xylophone app for iOS. Add sound player

---
+++ Xylophone/ VIewController.swift
@@ -0,0 +1,42 @@
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController{
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    var player: AVAudioPlayer?
+
+    @IBAction func notePressed(_ sender: UIButton) {
+        
+        guard let url = Bundle.main.url(forResource: "note1", withExtension: "wav")
+            else { return }
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            
+            /* iOS 10 and earlier require the following line:
+             player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+            
+            guard let player = player else { return }
+            
+            player.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        
+    }
+    
+  
+
+}
+
